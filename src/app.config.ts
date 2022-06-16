dc0bcbcc15e8fff9a044b1f47d59dd44167b2ba6
Ravuthz <yovannaravuth@gmail.com>
2022-06-16 02:54:46 +0000

    Add sqlite as default db

--- src/app.config.ts
+++ src/app.config.ts
@@ -2,12 +2,12 @@ export default () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
   debug: process.env.DEBUG || false,
   database: {
-    type: process.env.TYPE || 'postgresql',
+    type: process.env.DB_TYPE || 'sqlite',
     host: process.env.DB_HOST || 'localhost',
     port: parseInt(process.env.DB_PORT, 10) || 5432,
     user: process.env.DB_USER || 'postgres',
     password: process.env.DB_PASS || 'postgres',
-    dbName: process.env.DB_NAME || 'postgres',
+    dbName: process.env.DB_NAME || 'data.db',
     debug: process.env.DEBUG || false,
   },
 });
